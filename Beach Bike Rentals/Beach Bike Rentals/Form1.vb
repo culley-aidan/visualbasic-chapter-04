22955d38266bb5e58353a0c3d05c3e1859ed0320
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-27 09:53:47 -0500

    added comments

--- Beach Bike Rentals/Beach Bike Rentals/Form1.vb
+++ Beach Bike Rentals/Beach Bike Rentals/Form1.vb
@@ -1,5 +1,12 @@
-﻿Public Class frmBike
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+﻿' Program:  Beach Bike Rentls
+' Author:   Aidan Culley
+' Date:     August 27th, 2019
+' Purpose:  This application calculates and displays the total
+'           cost of renting a bike for a specified number of days.
+
+
+Public Class frmBike
+    Private Sub BtnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
 
     End Sub
 End Class
