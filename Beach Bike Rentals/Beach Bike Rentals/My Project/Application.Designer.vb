8707be3e7515b3f5df0f4909e7eda34d379fd122
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-27 08:50:52 -0500

    renamed frm

--- Beach Bike Rentals/Beach Bike Rentals/My Project/Application.Designer.vb
+++ Beach Bike Rentals/Beach Bike Rentals/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Beach_Bike_Rentals.Form1
+            Me.MainForm = Global.Beach_Bike_Rentals.frmBike
         End Sub
     End Class
 End Namespace
