e16cc91d2b6a87b4e1f3dbe4ba52c9a167d349c6
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-28 09:15:47 -0500

    adding introductory line

--- Seattle Walking Tour/Seattle Walking Tour/Form1.vb
+++ Seattle Walking Tour/Seattle Walking Tour/Form1.vb
@@ -1,2 +1,8 @@
-﻿Public Class frmSeattle
+﻿' Program:  Seattle Walking Tour
+' Author:   Aidan Culley
+' Date:     August 28th, 2019
+' Purpose:  This application calculates and displays the total
+'           cost of a number of tickets
+
+Public Class frmSeattle
 End Class
