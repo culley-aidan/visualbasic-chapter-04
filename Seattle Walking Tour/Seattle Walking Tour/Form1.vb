3241ff595b112ecbe4f435115332e7db79e86b07
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-28 09:23:06 -0500

    added logic for btnCost

--- Seattle Walking Tour/Seattle Walking Tour/Form1.vb
+++ Seattle Walking Tour/Seattle Walking Tour/Form1.vb
@@ -11,6 +11,16 @@ Public Class frmSeattle
     Const _bookingFee As Decimal = 1.99D
 
     Private Sub BtnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
+        ' Executes when btnCost is clicked
+        ' Calculates and displays cost of tickets and the one time booking fee
 
+        Dim strNumberOfTickets As String
+        Dim intNumberOfTickets As Integer
+        Dim decTotalCost As Decimal
+
+        strNumberOfTickets = txtTicketCount.Text
+        intNumberOfTickets = Convert.ToInt32(strNumberOfTickets)
+        decTotalCost = (intNumberOfTickets * _ticketPricePerTicket) + _bookingFee
+        lblTotalCost.Text = decTotalCost.ToString("C")
     End Sub
 End Class
