3d17d7ffdeac43e175d4d301635082413db4d4d0
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-28 09:19:02 -0500

    created consts and added cost button event

--- Seattle Walking Tour/Seattle Walking Tour/Form1.vb
+++ Seattle Walking Tour/Seattle Walking Tour/Form1.vb
@@ -6,4 +6,11 @@
 
 Option Strict On
 Public Class frmSeattle
+
+    Const _ticketPricePerTicket As Decimal = 14.99D
+    Const _bookingFee As Decimal = 1.99D
+
+    Private Sub BtnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
+
+    End Sub
 End Class
