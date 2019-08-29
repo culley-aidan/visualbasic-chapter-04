800ce169854a040c23123f9604c48da4c69f03e2
culley-aidan <culley-aidan@users.noreply.github.com>
2019-08-29 09:22:05 -0500

    added statetax const

--- Payroll Calculator/Payroll Calculator/Form1.vb
+++ Payroll Calculator/Payroll Calculator/Form1.vb
@@ -11,5 +11,7 @@ Public Class frmPayroll
     Const _ficaTaxPercentage As Decimal = (7.65D / 100)
     ' Federal Tax amount
     Const _federalTaxPercentage As Decimal = (22D / 100)
+    ' State tax amount
+    Const _stateTaxPercentage As Decimal = (4D / 100)
 
 End Class
