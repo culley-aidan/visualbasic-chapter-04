e703ab249ef0eca0edb7619c943ea92003bbd92a
culley-aidan <culley-aidan@users.noreply.github.com>
2019-08-29 08:42:53 -0500

    added introductory line

--- Payroll Calculator/Payroll Calculator/Form1.vb
+++ Payroll Calculator/Payroll Calculator/Form1.vb
@@ -1,2 +1,8 @@
-﻿Public Class frmPayroll
+﻿' Program:  Payroll Calculator
+' Author:   Aidan Culley
+' Date:     August 29th, 2019
+' Purpose:  This application calculates and displays FICA tax, federal tax, 
+'           and state tax for a two week pay period
+
+Public Class frmPayroll
 End Class
