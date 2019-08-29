9b0a5cf8d4afbf816f5d8360cf11051a64e8b4e2
culley-aidan <culley-aidan@users.noreply.github.com>
2019-08-29 09:47:05 -0500

    made the logic for btnCalculate

--- Payroll Calculator/Payroll Calculator/Form1.vb
+++ Payroll Calculator/Payroll Calculator/Form1.vb
@@ -14,4 +14,32 @@ Public Class frmPayroll
     ' State tax amount
     Const _stateTaxPercentage As Decimal = (4D / 100)
 
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Executes when btnCalculate is clicked
+        ' Calculates the tax deduction 
+
+        Dim strPaycheck As String
+        Dim decPaycheck As Decimal
+        Dim decTotal As Decimal
+
+        strPaycheck = txtPayCheck.Text
+        decPaycheck = Convert.ToInt32(strPaycheck)
+
+        Dim ficaTaxDeduction As Decimal = Math.Round(decPaycheck * _ficaTaxPercentage, 2)
+        Dim federalTaxDeduction As Decimal = Math.Round(decPaycheck * _federalTaxPercentage, 2)
+        Dim stateTaxDeduction As Decimal = Math.Round(decPaycheck * _stateTaxPercentage, 2)
+
+        lblFICATaxAmount.Text = String.Format("${0} ({1}%)", ficaTaxDeduction, Math.Round(_ficaTaxPercentage * 100, 2))
+        lblFederalTaxAmount.Text = String.Format("${0} ({1}%)", federalTaxDeduction, Math.Round(_federalTaxPercentage * 100, 2))
+        lblStateTaxAmount.Text = String.Format("${0} ({1}%)", stateTaxDeduction, Math.Round(_stateTaxPercentage * 100, 2))
+
+        decTotal = decPaycheck
+        decTotal -= ficaTaxDeduction
+        decTotal -= federalTaxDeduction
+        decTotal -= stateTaxDeduction
+        decTotal = Math.Round(decTotal, 2)
+
+        lblNetPayAmount.Text = String.Format("${0}", decTotal)
+        btnClear.Enabled = True
+    End Sub
 End Class
